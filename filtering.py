61c420846667902ce704266d981c316cd48f4146
Niko Wenselowski <der@nik0.de>
2014-10-21 09:13:59 +0200

    Testing also with lambda.

--- filtering.py
+++ filtering.py
@@ -22,6 +22,10 @@ def truth_testing_with_filter(numbers):
     return filter(None, numbers)
 
 
+def truth_testing_with_lambda_and_filter(numbers):
+    return filter(lambda x: x, numbers)
+
+
 def truth_testing_list_comprehension(numbers):
     return [number for number in numbers if number]
 
@@ -30,8 +34,10 @@ if __name__ == '__main__':
     elements = list(range(10) * 10)
 
     print("Elements: {0}".format(len(elements)))
-    print("Filter: {0}".format(timeit.timeit("filter_function(elements)", setup="from __main__ import filter_function, elements ")))
+    print("filter: {0}".format(timeit.timeit("filter_function(elements)", setup="from __main__ import filter_function, elements ")))
+    print("filter and lambda: {0}".format(timeit.timeit("filter_function_with_lambda(elements)", setup="from __main__ import filter_function_with_lambda, elements ")))
     print("List Comprehension: {0}".format(timeit.timeit("list_comprehension(elements)", setup="from __main__ import list_comprehension, elements")))
     print("Truth testing with filter: {0}".format(timeit.timeit("truth_testing_with_filter(elements)", setup="from __main__ import truth_testing_with_filter, elements ")))
+    print("Truth testing with filter and lambda: {0}".format(timeit.timeit("truth_testing_with_lambda_and_filter(elements)", setup="from __main__ import truth_testing_with_lambda_and_filter, elements ")))
     print("Truth testing with List Comprehension: {0}".format(timeit.timeit("truth_testing_list_comprehension(elements)", setup="from __main__ import truth_testing_list_comprehension, elements")))
 
