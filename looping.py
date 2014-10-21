c7050d913bb7f39116017b3d9e1795d111e7fef0
Niko Wenselowski <der@nik0.de>
2014-10-21 09:05:17 +0200

    Do not run tests on module import.

--- looping.py
+++ looping.py
@@ -24,9 +24,11 @@ def optimised_for_loop(input):
     return data
 
 
-elements = list(range(100))
+if __name__ == '__main__':
+    elements = list(range(100))
 
-print("Elements: {0}".format(len(elements)))
-print("For Loop: {0}".format(timeit.timeit("for_loop(elements)", setup="from __main__ import for_loop, elements ")))
-print("Optimised For Loop: {0}".format(timeit.timeit("optimised_for_loop(elements)", setup="from __main__ import optimised_for_loop, elements ")))
-print("List Comprehension: {0}".format(timeit.timeit("list_comprehension(elements)", setup="from __main__ import list_comprehension, elements")))
+    print("Elements: {0}".format(len(elements)))
+    print("For Loop: {0}".format(timeit.timeit("for_loop(elements)", setup="from __main__ import for_loop, elements ")))
+    print("Optimised For Loop: {0}".format(timeit.timeit("optimised_for_loop(elements)", setup="from __main__ import optimised_for_loop, elements ")))
+    print("List Comprehension: {0}".format(timeit.timeit("list_comprehension(elements)", setup="from __main__ import list_comprehension, elements")))
+
